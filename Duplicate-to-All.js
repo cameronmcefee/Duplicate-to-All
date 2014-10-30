3054b8a0a3e861012c453a4a76375b34f2f85278
Cameron McEfee <cameron@github.com>
2014-10-30 09:22:08 -0700

    whitespace

--- Duplicate-to-All.js
+++ Duplicate-to-All.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
      o----------------------o----------------------o
              CAMERON McEFEE | 530.339.8372
@@ -7,48 +7,48 @@
                    Thanks for looking!
 
  Duplicate-To-All is a Photoshop script by Cameron McEfee.
-			 
+
                   www.cameronmcefee.com
-				 
+
 */
 
 if( app.documents.length > 0 ){
-	duplicateToAll();
+  duplicateToAll();
 }
 function duplicateToAll(){
-	docs = app.documents;
-	curDoc = app.activeDocument;
-	groupLayerset();
-	for(var i = 0; i < docs.length; i++){
-		if(curDoc != docs[i]){
-			try {
-				var curLayer = docs[i].activeLayer;
-				curDoc.activeLayer.duplicate(docs[i],ElementPlacement.PLACEATBEGINNING);
-				app.activeDocument = docs[i];
-				app.activeDocument.activeLayer.name = 'Duplicate To All Group';
-				docs[i].activeLayer.move(curLayer, ElementPlacement.PLACEBEFORE);
-				ungroupLayerset();
-			} catch(e) {
-				alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [duplicateToAll()] \n\n' + e);
-			}
+  docs = app.documents;
+  curDoc = app.activeDocument;
+  groupLayerset();
+  for(var i = 0; i < docs.length; i++){
+    if(curDoc != docs[i]){
+      try {
+        var curLayer = docs[i].activeLayer;
+        curDoc.activeLayer.duplicate(docs[i],ElementPlacement.PLACEATBEGINNING);
+        app.activeDocument = docs[i];
+        app.activeDocument.activeLayer.name = 'Duplicate To All Group';
+        docs[i].activeLayer.move(curLayer, ElementPlacement.PLACEBEFORE);
+        ungroupLayerset();
+      } catch(e) {
+        alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [duplicateToAll()] \n\n' + e);
+      }
 
-		}
-		app.activeDocument = curDoc;
-	}
-	ungroupLayerset();
-	alert('"Duplicate to All" complete');
+    }
+    app.activeDocument = curDoc;
+  }
+  ungroupLayerset();
+  alert('"Duplicate to All" complete');
 }
 function ungroupLayerset(){
-	var m_Dsc01 = new ActionDescriptor();
-	var m_Ref01 = new ActionReference();
-	m_Ref01.putEnumerated( cTID( "Lyr " ), cTID( "Ordn" ), cTID( "Trgt" ) );
-	m_Dsc01.putReference( cTID( "null" ), m_Ref01 );
-	
-	try {
-		executeAction( sTID( "ungroupLayersEvent" ), m_Dsc01, DialogModes.NO );
-	} catch(e) {
-		alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [ungroupLayerset()] \n\n' + e);	
-	}
+  var m_Dsc01 = new ActionDescriptor();
+  var m_Ref01 = new ActionReference();
+  m_Ref01.putEnumerated( cTID( "Lyr " ), cTID( "Ordn" ), cTID( "Trgt" ) );
+  m_Dsc01.putReference( cTID( "null" ), m_Ref01 );
+
+  try {
+    executeAction( sTID( "ungroupLayersEvent" ), m_Dsc01, DialogModes.NO );
+  } catch(e) {
+    alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [ungroupLayerset()] \n\n' + e);
+  }
 }
 function cTID(s){return charIDToTypeID(s)}
 function sTID(s){return stringIDToTypeID(s)}
@@ -61,8 +61,8 @@ function groupLayerset(){
     ref.putEnumerated( charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt') );
     desc.putReference( charIDToTypeID('From'), ref );
     try {
-		executeAction( charIDToTypeID('Mk  '), desc, DialogModes.NO );
-	} catch(e) {
-		alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [groupLayerset()] \n\n' + e);
-	}
+    executeAction( charIDToTypeID('Mk  '), desc, DialogModes.NO );
+  } catch(e) {
+    alert('"Duplicate to All" has encountered an error. Please email the text below to cm@cameronmcefee.com \n' + 'Error with PS Script: Duplicate to All at [groupLayerset()] \n\n' + e);
+  }
 };
